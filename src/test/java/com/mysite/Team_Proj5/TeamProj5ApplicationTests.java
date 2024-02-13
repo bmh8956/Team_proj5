0cfeec5d61ec182dd9993dbc82c3c5d3e5ec187c
minhyeok Bang <bmh89895656@gmail.com>
2024-02-13 18:07:22 +0900

    최초 커밋

---
+++ src/test/java/com/mysite/Team_Proj5/TeamProj5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.Team_Proj5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
